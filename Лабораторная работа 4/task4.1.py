5f3c2d1d5bb25b4e201ad87e02fa4f12b9974a6c
kzkvaan <kzkvaar@gmail.com>
2024-12-21 22:24:46 +0500

    Add files via upload

---
+++ Лабораторная работа 4/task4.1.py
@@ -0,0 +1,13 @@
+# TODO решите задачу
+
+import json
+
+def task() -> float:
+    filename = "input.json"
+    with open(filename) as f:
+        json_data = json.load(f)
+
+    summ = sum ([item["score"] * item["weight"] for item in json_data])
+    return round( summ, 3)
+
+print(task())
